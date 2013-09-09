90660f30889ef24cced6278dd2862deacb9d29b9
David Williamson <david@davidized.com>
2013-09-08 21:12:19 -0500

    Added utility script to create symlinks to Dropbox folders in home directory

---
+++ utils/dropbox_init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ~/
+
+# Yearly archives (last year and this year)
+ln -s ~/Dropbox/2012 ~/2012
+ln -s ~/Dropbox/2013 ~/2013
+
+ln -s ~/Dropbox/Hobbies ~/Hobbies
+ln -s ~/Dropbox/Projects ~/Projects
+ln -s ~/Dropbox/ToRead ~/Downloads/ToRead
+
