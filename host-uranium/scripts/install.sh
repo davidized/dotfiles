19313599d02f57eec0d683acf7e6667e20eda502
David Williamson <david@davidized.com>
2014-07-03 20:28:00 -0500

    Update brewfiles and git aliases

--- host-uranium/scripts/install.sh
+++ host-uranium/scripts/install.sh
@@ -37,29 +37,7 @@ chsh -s /usr/local/bin/zsh
 echo "Host Uranium: Set some default options"
 bash $HOME/.scripts/osx.setdefaults.
 
-# Link directories
-link_directories() {
-  # params: link to, link from
-  if [ -h $1 ] # Destination is already a symlink
-    then
-    echo "Relinking $1"
-    rm "$1"
-    ln -s "$2" "$1"
-  elif [ ! -e "$1" ] # Check if destination exists
-    then
-    echo "Linking $1 to $2"
-    ln -s "$2" "$1"
-  else
-    echo "$1 already exists (not a symlink), skipping."
-  fi
-}
-
-echo "Host Uranium: Link directories"
-link_directories $HOME/2014              $OWNCLOUD/2014
-link_directories $HOME/Hobbies           $OWNCLOUD/Hobbies
-link_directories $HOME/Projects          $OWNCLOUD/Projects
-link_directories $HOME/Downloads/ToRead  $DROPBOX/ToRead
-#link_directories $HOME/.secret/gnupg     $HOME/.gnupg
+# linkdirs.sh
 
 # Tag GPG
 # Symlink gpg2 to gpg??
--- host-uranium/zsh/host-uranium.zshenv
+++ host-uranium/zsh/host-uranium.zshenv
@@ -1,3 +1,5 @@
 export DROPBOX=$HOME/Dropbox
 export OWNCLOUD=$HOME/OwnCloud
 export VISUAL=atom
+export PATH=/usr/texbin:$PATH
+export ATOM_PATH=/Users/david/Applications/Atom.app
