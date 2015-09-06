94a1a5ca8bbf51f376f10800bc49685e9f76eaf0
David Williamson <david@davidized.com>
2015-09-06 13:59:35 -0500

    Update linking routines

--- uranium.local/linkdirs.sh
+++ uranium.local/linkdirs.sh
@@ -18,11 +18,12 @@ link_directories() {
 }
 
 echo "Host Uranium: Link directories"
-link_directories $HOME/2014              $DROPBOX/2014
+link_directories $HOME/2015              $DROPBOX/2015
 link_directories $HOME/Hobbies           $DROPBOX/Hobbies
 link_directories $HOME/Projects          $DROPBOX/Projects
 link_directories $HOME/Downloads/ToRead  $DROPBOX/ToRead
 
 link_directories $HOME/Papers            $DROPBOX/Papers
 #link_directories $HOME/PersonalWiki      $DROPBOX/PersonalWiki
-link_directories $HOME/.secret/gnupg     $HOME/.gnupg
+link_directories $HOME/.gnupg            $HOME/.secret/gnupg
+link_directories $HOME/.ssh              $HOME/.secret/ssh     
